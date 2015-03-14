87da90de41af8e6930ef3e92da03f3c5d4b2e9ff
mrsufgi <sufgis@gmail.com>
2015-03-14 17:38:43 +0200

    initial merge
    might need to revert back. stay clear!

---
+++ aaa_game_v0.0/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	void OnGUI()
+	{
+
+		if (GUI.Button (new Rect (10, 10, 150, 100), "Play")) 
+		{
+			Application.LoadLevel(0);
+		}
+		if (GUI.Button (new Rect (10, 150, 150, 100), "Quit")) 
+		{
+			Application.Quit();
+		}
+	}
+}
